5e5fd3048913f62b3e06d444223bd47ca20eed4e
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-01-05 01:36:00 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,12 +14,12 @@ end
 #=> "The line is currently: 1. Ada 2. Grace 3. Kent"
 def line(katz_deli)
 
-  line_string = "The line is currently: "
+  line_string = "The line is currently:"
   if  katz_deli[0] == "" || katz_deli[0] == " "  || katz_deli[0] == nil
     line_string = "The line is currently empty."
   else
     katz_deli.each_with_index do |name, index|
-        line_string << "#{index + 1}. #{name} "
+        line_string << " #{index + 1}. #{name} "
     end
   end
   puts line_string
