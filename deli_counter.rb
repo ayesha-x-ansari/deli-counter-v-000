caaa4d64d90c0cbf572c95e275ae6ab60d61659f
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-01-05 01:00:02 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,26 @@
 # Write your code here.
+katz_deli = []
+name = ""
+
+#=> Welcome, Ada. You are number 1 in line.
+def take_a_number(katz_deli, name)
+  katz_deli << name
+  katz_deli.each_with_index do |name, index|
+     "Welcome, #{name}. You are number #{index} in line."
+  end
+end
+#take_a_number(katz_deli, name)
+
+#=> "The line is currently: 1. Ada 2. Grace 3. Kent"
+def line(katz_deli)
+  line_string = "The line is currently: "
+  if [katz_deli].empty?
+    line_string "The line is currently empty."
+  else
+    katz_deli.each_with_index do |name, index|
+       line_string << "#{index + 1}. #{name} "
+    end
+  end
+  puts  line_string
+end
+line(katz_deli)
