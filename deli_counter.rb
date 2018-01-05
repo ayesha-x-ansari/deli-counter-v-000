88470499411c2a99187809cc469c59c22d63ae58
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-01-05 01:41:01 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,7 +6,7 @@ name = ""
 def take_a_number(katz_deli, name)
   katz_deli << name
   katz_deli.each_with_index do |name, index|
-     "Welcome, #{name}. You are number #{index} in line."
+     puts  "Welcome, #{name}. You are number #{index} in line."
   end
 end
 #take_a_number(katz_deli, name)
